d0c6588bc8a49aad32063965d072033609015842
lucaspereira21 <lucasmogisp02@outlook.com>
2023-09-29 15:15:01 -0300

    Content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"NOVO HOMEM DE FERRO ANUNCIADO",
+    "description":"Marvel studios Anuncia novo filme de homem de ferro",
+    "photocover":"https://is3-ssl.mzstatic.com/image/thumb/ZypiNEdbU0wwCF0GMJ3zoA/1200x675mf.jpg"
+  },
+  {
+    "id":"2",
+    "title":"Nova temporada de Loki estreia em 5 de outubro de 2023",
+    "description":"confira!",
+    "photocover":"https://legadodamarvel.com.br/wp-content/uploads/2023/09/8-filmes-e-series-da-Marvel-para-assistir-antes-da-2%C2%B0-temporada-de-Loki-legadodamarvel.webp"
+  },
+  {
+    "id":"3",
+    "title":"Vingadores: Ultimato: Marvel esclarece o último mistério sobre o Capitão América velho",
+    "description":"confira!",
+    "photocover":"https://legadodamarvel.com.br/wp-content/uploads/2023/09/Vingadores-Ultimato-Marvel-esclarece-o-ultimo-misterio-sobre-o-Capitao-America-velho-legadodamarvel.webp"
+  },
+  {
+    "id":"4",
+    "title":"Marvel revela a surpreendente idade da Capitã Marvel",
+    "description":"confira!",
+    "photocover":"https://www.otempo.com.br/image/contentid/policy:1.3241745:1695749770/image-Marvel-revela-a-surpreendente-idade-da-Capita-Marvel.jpg?f=3x2&w=1224l"
+  }
+]
